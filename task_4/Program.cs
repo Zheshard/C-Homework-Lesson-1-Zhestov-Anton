e4620a8def043c21117662b4acd2c41c11e0d1e6
Zheshard <antonzestkov489@gmail.com>
2022-12-02 13:23:51 +0300

    completed task 4

---
+++ task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int index = 2;
+
+while (index <= number)
+{
+	Console.WriteLine(index);
+	index += 2;
+}
