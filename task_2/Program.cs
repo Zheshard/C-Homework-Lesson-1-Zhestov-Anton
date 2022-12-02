b1ff7bc660eda5ba164b37f2e8b50adecdf145d4
Zheshard <antonzestkov489@gmail.com>
2022-12-02 12:52:52 +0300

    completed task 2

---
+++ task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа, которая принимает на вход 3 числа и выдает максимальное из этих чисел.
+Console.WriteLine("Введите 3 числа:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (max < number2)
+{
+	max = number2;
+}
+if (max < number3)
+{
+	max = number3;
+}
+
+Console.WriteLine("Максимальнео чиcло равно " + max);
