3c169d87f2608b2708abebcbe2e3e183c627b802
Zheshard <antonzestkov489@gmail.com>
2022-12-02 12:32:28 +0300

    completed task 1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите 2 целых числа: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (number_1 > number_2)
+{
+	Console.Write("Число 1 (" + number_1 + ") больше числа 2 (" + number_2 + ")");
+}
+else if (number_1 == number_2)
+{
+	Console.Write("Число 1 (" + number_1 + ") равно числу 2 (" + number_2 + ")");
+}
+else
+{
+	Console.Write("Число 1 (" + number_1 + ") меньше числа 2 (" + number_2 + ")");
+}
